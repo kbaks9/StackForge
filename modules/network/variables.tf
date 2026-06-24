94a6486dd5ce18a3e25f0245de77b73c7b110c80
kbaks9 <kamaal.bakar@outlook.com>
2026-06-24 18:46:24 +0100

    Added variables for network/main.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,48 @@
+variable "resource_group" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "network_name" {
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "vnet_address_space" {
+  type        = string
+  description = "Virtual network address space"
+}
+
+variable "subnet_web_name" {
+  type        = string
+  description = "Name of the web subnet"
+}
+
+variable "subnet_app_name" {
+  type        = string
+  description = "Name of the app subnet"
+}
+
+variable "subnet_data_name" {
+  type        = string
+  description = "Name of the data subnet"
+}
+
+variable "prefix_web" {
+  type        = string
+  description = "Web subnet address prefix"
+}
+
+variable "prefix_app" {
+  type        = string
+  description = "App subnet address prefix"
+}
+
+variable "prefix_data" {
+  type        = string
+  description = "Data subnet address prefix"
+}
