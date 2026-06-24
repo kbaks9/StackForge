3d34856108fd96a2af9463359126671ae6c964f6
kbaks9 <kamaal.bakar@outlook.com>
2026-06-24 23:08:23 +0100

    Update: split the nsg module into 3, created a nsg for subnet so it would be more clear.

--- main.tf
+++ main.tf
@@ -9,22 +9,26 @@ module "network" {
   location           = var.location
   network_name       = var.network_name
   vnet_address_space = var.vnet_address_space
-  subnet_web_name    = var.subnet_web_name
-  subnet_app_name    = var.subnet_app_name
-  subnet_data_name   = var.subnet_data_name
-  prefix_web         = var.prefix_web
-  prefix_app         = var.prefix_app
-  prefix_data        = var.prefix_data
-  security_group     = module.nsg.nsg_id
+
+  subnet_web_name  = var.subnet_web_name
+  subnet_app_name  = var.subnet_app_name
+  subnet_data_name = var.subnet_data_name
+
+  prefix_web  = var.prefix_web
+  prefix_app  = var.prefix_app
+  prefix_data = var.prefix_data
+
+  security_group_web  = module.nsg_web.nsg_id
+  security_group_app  = module.nsg_app.nsg_id
+  security_group_data = module.nsg_data.nsg_id
 }
 
-module "nsg" {
+module "nsg_web" {
   source         = "./modules/nsg"
   resource_group = azurerm_resource_group.rg-grp.name
   location       = azurerm_resource_group.rg-grp.location
-  nsg_web_name   = var.nsg_web_name
-  nsg_app_name   = var.nsg_app_name
-  nsg_data_name  = var.nsg_data_name
+
+  nsg_name = var.nsg_web_name
   rules = [
     {
       name                       = "Allow_Admin_SSH"
@@ -61,8 +65,16 @@ module "nsg" {
       destination_port_range     = "443"
       source_address_prefix      = "Internet"
       destination_address_prefix = "10.0.1.0/24"
-    },
+  }]
+}
 
+module "nsg_app" {
+  source         = "./modules/nsg"
+  resource_group = azurerm_resource_group.rg-grp.name
+  location       = azurerm_resource_group.rg-grp.location
+
+  nsg_name = var.nsg_app_name
+  rules = [
     {
       name                       = "Allow_Web_To_App" # For Web -> App subnet
       priority                   = 130
@@ -74,10 +86,18 @@ module "nsg" {
       source_address_prefix      = "10.0.1.0/24"
       destination_address_prefix = "10.0.2.0/24"
 
-    },
+  }]
+}
 
+module "nsg_data" {
+  source         = "./modules/nsg"
+  resource_group = azurerm_resource_group.rg-grp.name
+  location       = azurerm_resource_group.rg-grp.location
+
+  nsg_name = var.nsg_data_name
+  rules = [
     {
-      name                       = "Allow_App_To_Data"
+      name                       = "Allow_App_To_Data" # Allow App traffic -> Data
       priority                   = 140
       direction                  = "Inbound"
       access                     = "Allow"
@@ -87,11 +107,6 @@ module "nsg" {
       source_address_prefix      = "10.0.2.0/24"
       destination_address_prefix = "10.0.3.0/24"
   }]
-
-
-  depends_on = [
-    azurerm_resource_group.rg-grp
-  ]
 }
 
 # module "vm" {
