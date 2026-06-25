b6763f8980d1d39ff8c4b7bc4c38aaa7fc9d5620
kbaks9 <kamaal.bakar@outlook.com>
2026-06-25 01:01:42 +0100

    removed inline nest blocks to separate resources

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,27 +3,29 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = var.resource_group
   location            = var.location
   address_space       = [var.vnet_address_space]
-  #dns_servers         = ["10.0.0.4", "10.0.0.5"] 
-
-  subnet {
-    name             = var.subnet_web_name # subnet_web
-    address_prefixes = [var.prefix_web]
-    security_group   = var.security_group_web
-  }
-
-  subnet {
-    name             = var.subnet_app_name # subnet_app
-    address_prefixes = [var.prefix_app]
-    security_group   = var.security_group_app
-  }
-
-  subnet {
-    name             = var.subnet_data_name # subnet_data
-    address_prefixes = [var.prefix_data]
-    security_group   = var.security_group_data
-  }
 
   tags = {
     environment = "Dev"
   }
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = var.subnet_web_name
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.prefix_web]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = var.subnet_app_name
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.prefix_app]
+}
+
+resource "azurerm_subnet" "data" {
+  name                 = var.subnet_data_name
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.prefix_data]
 }
