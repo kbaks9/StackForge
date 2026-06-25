8b5fdb56268a539dd34489e060ea072747369ce1
kbaks9 <kamaal.bakar@outlook.com>
2026-06-25 17:21:12 +0100

    Added dynamic creation of admin ssh keys, nic public ip, linked the nic ip to the ip config.

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -3,11 +3,22 @@ resource "azurerm_network_interface" "nic" {
   location            = var.location
   resource_group_name = var.resource_group
 
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.create_public_ip ? azurerm_public_ip.nic-pip[0].id : null
   }
+}
+
+resource "azurerm_public_ip" "nic-pip" {
+  count               = var.create_public_ip ? 1 : 0
+  name                = "public-ip-${var.vm_name}"
+  location            = var.location
+  resource_group_name = var.resource_group
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 
@@ -16,14 +27,20 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name = var.resource_group
   location            = var.location
   size                = "Standard_B1s"
-  admin_username      = "adminuser"
+  admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+  disable_password_authentication = var.ssh_public_key != null
+  dynamic "admin_ssh_key" {
+    # If an SSH key is not null (which means it exists): create one admin_ssh_key block
+    # Otherwise: create zero admin_ssh_key blocks
+    for_each = var.ssh_public_key != null ? [var.ssh_public_key] : []
+    content {
+      username   = var.admin_username
+      public_key = admin_ssh_key.value
+    }
   }
 
   os_disk {
